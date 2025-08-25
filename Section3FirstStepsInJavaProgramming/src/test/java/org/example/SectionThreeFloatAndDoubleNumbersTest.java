53d3073f81c377d5c3f3593d7c5f3022fd86baa6
Eric Ricketts <eric_ricketts@me.com>
2025-08-25 19:23:04 -0400

    first workings with floating point numbers

--- Section3FirstStepsInJavaProgramming/src/test/java/org/example/SectionThreeFloatAndDoubleNumbersTest.java
+++ Section3FirstStepsInJavaProgramming/src/test/java/org/example/SectionThreeFloatAndDoubleNumbersTest.java
@@ -1,23 +1,61 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class SectionThreeFloatAndDoubleNumbersTest {
+
+    Float expectedFloat;
+    Double expectedDouble;
 
     @Test
     void testFloatVariable() {
         // had to declare Float because default decimal format is Double
-        Float expected = 5.25F;
+        expectedFloat = 5.25F;
         float  myFloatVariable = 5.25F;
-        assertEquals(expected, myFloatVariable);
+        Assertions.assertEquals(myFloatVariable, expectedFloat);
     }
 
     @Test
     void testDoubleVariable() {
         // since Double is default for decimal numbers no need to use "d" or "D" suffix
-        Double expected = 5.25D;
+        expectedDouble = 5.25D;
         double  myDoubleVariable = 5.25D;
-        assertEquals(expected, myDoubleVariable);
+        Assertions.assertEquals(myDoubleVariable, expectedDouble);
+    }
+
+    @Test
+    void testCastingForAFloat() {
+        // note we did the cast because the default real number type is double
+        // obviously casting is too wordy, better is just standard type declaration
+        // expectedFloat = 5.25F;
+        expectedFloat = (float) 5.25;
+        float  myFloatVariable = (float) 5.25;
+        Assertions.assertEquals(myFloatVariable, expectedFloat);
+    }
+
+    @Test
+    void testSimpleIntegerDivision() {
+        // for integer division Java does some special rounding
+        int firstInteger = 5;
+        int secondInteger = 2;
+        int thirdInteger = 3;
+        Assertions.assertEquals(secondInteger, firstInteger / secondInteger);
+        Assertions.assertEquals(1, firstInteger / thirdInteger);
+    }
+
+    @Test
+    void testSimpleDoubleDivision() {
+        Double firstDouble = 5.00;
+        Double secondDouble = 3.00;
+        BigDecimal answer;
+        answer = BigDecimal.valueOf(1.0 + 2.0 / 3.0);
+        BigDecimal truncatedAnswer = answer.setScale(6, RoundingMode.DOWN);
+        BigDecimal bigDecimalNumber = BigDecimal.valueOf(firstDouble / secondDouble);
+        BigDecimal bigDecimalNumberTruncated = bigDecimalNumber.setScale(6, RoundingMode.DOWN);
+        Assertions.assertEquals(truncatedAnswer,  bigDecimalNumberTruncated);
     }
 }
