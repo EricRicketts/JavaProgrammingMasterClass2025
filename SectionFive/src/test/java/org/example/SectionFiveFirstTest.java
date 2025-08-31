aa4fb1527f0d673c6e76c4dee9f0156dd97084ee
Eric Ricketts <eric_ricketts@me.com>
2025-08-31 07:04:57 -0400

    First commit for Section Five

---
+++ SectionFive/src/test/java/org/example/SectionFiveFirstTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SectionFiveFirstTest {
+
+    @Test
+    public void testForGit() {
+        // this test is just done to compile and push to remote repository
+        int myInt = 1;
+        Assertions.assertEquals(1, myInt);
+    }
+}
