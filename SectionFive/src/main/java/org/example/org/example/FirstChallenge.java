f8c19d361391de1fe7c4dfd12e557c429d2467f3
Eric Ricketts <eric_ricketts@me.com>
2025-09-05 07:10:09 -0400

    First Challenge completed, verified by JUnit testing

---
+++ SectionFive/src/main/java/org/example/org/example/FirstChallenge.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class FirstChallenge {
+
+    public String displayHighScorePosition(String name, int position) {
+        String s = STR."\{name} managed to get into position \{position} on the high score list.";
+        return s;
+    }
+
+    public int calculateHighScorePosition(int score) {
+        int position;
+        if (score >= 1000) {
+            position = 1;
+        } else if (score >= 500) {
+            position = 2; 
+        } else if (score >= 100) {
+            position = 3; 
+        } else {
+            position = 4;
+        }
+        
+        return position;
+    }
+
+}
---
+++ SectionFive/src/test/java/org/example/SectionFiveFirstChallengeTest.java
@@ -0,0 +1,34 @@
+import org.example.FirstChallenge;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SectionFiveFirstChallengeTest {
+    // scores to test: 1500, 1000, 500, 100 and 25
+    static FirstChallenge firstChallenge;
+    String[] names = {"Tim", "Bill", "Fred", "Elmer", "Sam"};
+    String[] expected = {
+            "Tim managed to get into position 1 on the high score list.",
+            "Bill managed to get into position 1 on the high score list.",
+            "Fred managed to get into position 2 on the high score list.",
+            "Elmer managed to get into position 3 on the high score list.",
+            "Sam managed to get into position 4 on the high score list."
+    };
+    int[] scores = {1500, 1000, 500, 100, 25};
+
+    @BeforeAll
+    public static void setup() {
+        firstChallenge = new FirstChallenge();
+    }
+
+    @Test
+    public void testScoresAndPositions() {
+        for (int i = 0; i < scores.length; i++) {
+            int position = firstChallenge.calculateHighScorePosition(scores[i]);
+            Assertions.assertEquals(
+                    expected[i],
+                    firstChallenge.displayHighScorePosition(names[i], position)
+            );
+        }
+    }
+}
