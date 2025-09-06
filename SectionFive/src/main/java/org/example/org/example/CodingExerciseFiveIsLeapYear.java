843a78d4869d6df63b71bfe22a5c244970958cef
Eric Ricketts <eric_ricketts@me.com>
2025-09-06 13:26:16 -0400

    simplify the logic for the leap year exercise

--- SectionFive/src/main/java/org/example/org/example/CodingExerciseFiveIsLeapYear.java
+++ SectionFive/src/main/java/org/example/org/example/CodingExerciseFiveIsLeapYear.java
@@ -3,11 +3,6 @@ package org.example;
 public class CodingExerciseFiveIsLeapYear {
 
     public boolean isLeapYear(int year) {
-        boolean leap;
-        if (year < 1 || year > 9999) { leap = false; }
-        else {
-            leap = (year % 4 == 0 &&  year % 100 != 0) || (year % 400 == 0);
-        }
-        return leap;
+        return (year >= 1 && year <= 9999) && (year % 4 == 0 &&  year % 100 != 0) || (year % 400 == 0);
     }
 }
