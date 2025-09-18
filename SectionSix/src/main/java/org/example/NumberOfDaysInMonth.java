66c2b055e24f14c233ed5c4e20150ce300871bca
Eric Ricketts <eric_ricketts@me.com>
2025-09-18 17:50:11 -0400

    use advanced switch syntax

--- SectionSix/src/main/java/org/example/NumberOfDaysInMonth.java
+++ SectionSix/src/main/java/org/example/NumberOfDaysInMonth.java
@@ -11,19 +11,13 @@ public class NumberOfDaysInMonth {
     public static int getDaysInMonth(int month, int year) {
         if ((month < 1 || month > 12) || (year < 1 || year > 9999)) return -1;
         return switch (month) {
-            case 1, 3, 5, 7, 8, 10, 12 -> {
-                yield 31;
-            }
-            case 4, 6, 9, 11 -> {
-                yield 30;
-            }
+            case 1, 3, 5, 7, 8, 10, 12 -> 31;
+            case 4, 6, 9, 11 -> 30;
             case 2 -> {
                 if (isLeapYear(year)) yield 29;
                 yield 28;
             }
-            default -> {
-                    yield -1;
-            }
+            default -> -1;
         };
     }
 }
