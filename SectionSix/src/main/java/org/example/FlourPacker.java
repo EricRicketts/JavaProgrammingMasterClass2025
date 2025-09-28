5bfd914cd24a37f26b452f4ed00553b291942eb8
Eric Ricketts <eric_ricketts@me.com>
2025-09-28 14:49:30 -0400

    need to work out the algorithm

---
+++ SectionSix/src/main/java/org/example/FlourPacker.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class FlourPacker {
+
+    public static int kilosPerBigCount = 5;
+    public static int  kilosPerSmallCount = 1;
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        boolean canPack = false;
+        if (bigCount < 0 || smallCount < 0 || goal < 0) return false;
+        int bigCountWeight = bigCount * kilosPerBigCount;
+        int smallCountWeight = smallCount * kilosPerSmallCount;
+        int totalWeight =  bigCountWeight + smallCountWeight;
+        if (totalWeight < goal) return false;
+
+
+        return canPack;
+    }
+}
---
+++ SectionSix/src/test/java/org/example/FlourPackerTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FlourPackerTest {
+
+    boolean result;
+    /*
+    @Test
+    public void testCanPackInvalidPositiveValues() {
+        result = FlourPacker.canPack(1, 0, 4);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testCanPackValidValues() {
+        result = FlourPacker.canPack(-1, 4, 4);
+        Assertions.assertFalse(result);
+    }
+*/
+    @Test
+    public void testCanPackOneZeroAndFive() {
+        result = FlourPacker.canPack(1, 0, 5);
+        Assertions.assertTrue(result);
+    }
+}
