4193d896dc61c3078d7bfb8dcbd618e018feb69f
Eric Ricketts <eric_ricketts@me.com>
2025-09-30 06:57:56 -0400

    Flour packer

--- SectionSix/src/main/java/org/example/FlourPacker.java
+++ SectionSix/src/main/java/org/example/FlourPacker.java
@@ -6,13 +6,21 @@ public class FlourPacker {
     public static int  kilosPerSmallCount = 1;
     public static boolean canPack(int bigCount, int smallCount, int goal) {
         boolean canPack = false;
+        int smallCountUsed = 0;
         if (bigCount < 0 || smallCount < 0 || goal < 0) return false;
         int bigCountWeight = bigCount * kilosPerBigCount;
         int smallCountWeight = smallCount * kilosPerSmallCount;
         int totalWeight =  bigCountWeight + smallCountWeight;
         if (totalWeight < goal) return false;
 
-
+        while (smallCountUsed <= smallCount) {
+            totalWeight = bigCountWeight + smallCountUsed *  kilosPerSmallCount;
+            if  (totalWeight == goal) {
+                canPack = true;
+                break;
+            }
+            smallCountUsed++;
+        }
         return canPack;
     }
 }
--- SectionSix/src/test/java/org/example/FlourPackerTest.java
+++ SectionSix/src/test/java/org/example/FlourPackerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 public class FlourPackerTest {
 
     boolean result;
-    /*
     @Test
     public void testCanPackInvalidPositiveValues() {
         result = FlourPacker.canPack(1, 0, 4);
@@ -18,10 +17,33 @@ public class FlourPackerTest {
         result = FlourPacker.canPack(-1, 4, 4);
         Assertions.assertFalse(result);
     }
-*/
     @Test
     public void testCanPackOneZeroAndFive() {
         result = FlourPacker.canPack(1, 0, 5);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testCanPackZeroFiveFour() {
+        result = FlourPacker.canPack(0, 5, 4);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testCanPackTwoTwoEleven() {
+        result = FlourPacker.canPack(2, 2, 11);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testCanPackOneZeroSix() {
+        result = FlourPacker.canPack(1, 0, 6);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testCanPackZeroFiveSix() {
+        result = FlourPacker.canPack(3, 7, 21);
         Assertions.assertTrue(result);
     }
 }
