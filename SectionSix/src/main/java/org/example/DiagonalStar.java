ce22ebf618b5c111e2be94039df158a6715ed6cf
Eric Ricketts <eric_ricketts@me.com>
2025-10-04 10:40:17 -0400

    square star

--- SectionSix/src/main/java/org/example/DiagonalStar.java
+++ SectionSix/src/main/java/org/example/DiagonalStar.java
@@ -2,7 +2,37 @@ package org.example;
 
 public class DiagonalStar {
 
-    public static String printStarSquare(int number) {
-        return "foo";
+    public static String printSquareStar(int number) {
+        String squareStar = "";
+        String star = "*";
+        String space = " ";
+        if (number < 5) return "Invalid Value";
+        for (int row = 1; row <= number; row++) {
+            if (row == 1 || row == number) {
+                squareStar = squareStar.concat(star.repeat(number) + "\n");
+                continue;
+            }
+            for  (int col = 1; col <= number; col++) {
+                if (col == 1) {
+                    squareStar = squareStar.concat(star);
+                } else if (col == number) {
+                    squareStar = squareStar.concat(star) + "\n";
+                } else if (row == col) {
+                    squareStar = squareStar.concat(star);
+                }  else if (col == number - row + 1) {
+                    squareStar = squareStar.concat(star);
+                } else {
+                    squareStar = squareStar.concat(space);
+                }
+            }
+        }
+        /*
+            *****
+            ** **
+            * * *
+            ** **
+            *****
+        */
+        return squareStar;
     }
 }
--- SectionSix/src/test/java/org/example/DiagonalStarTest.java
+++ SectionSix/src/test/java/org/example/DiagonalStarTest.java
@@ -1,4 +1,56 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DiagonalStarTest {
+
+    String expected, result;
+
+    @Test
+    public void testDiagonalStar() {
+        Assertions.assertEquals("Invalid Value", DiagonalStar.printSquareStar(4));
+    }
+
+    @Test
+    public void testDiagonalStarFive() {
+        expected = "*****\n" + "** **\n" + "* * *\n" + "** **\n" + "*****\n";
+        result = DiagonalStar.printSquareStar(5);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDiagonalStarEight() {
+        expected = "********\n" + "**    **\n" + "* *  * *\n" + "*  **  *\n" + "*  **  *\n" +
+        "* *  * *\n" +  "**    **\n" + "********\n";
+        result = DiagonalStar.printSquareStar(8);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDiagonalStarTwenty() {
+        expected =
+        "********************\n" +
+        "**                **\n" +
+        "* *              * *\n" +
+        "*  *            *  *\n" +
+        "*   *          *   *\n" +
+        "*    *        *    *\n" +
+        "*     *      *     *\n" +
+        "*      *    *      *\n" +
+        "*       *  *       *\n" +
+        "*        **        *\n" +
+        "*        **        *\n" +
+        "*       *  *       *\n" +
+        "*      *    *      *\n" +
+        "*     *      *     *\n" +
+        "*    *        *    *\n" +
+        "*   *          *   *\n" +
+        "*  *            *  *\n" +
+        "* *              * *\n" +
+        "**                **\n" +
+        "********************\n";
+        result = DiagonalStar.printSquareStar(20);
+        Assertions.assertEquals(expected, result);
+    }
 }
