53cbd1cc5e0e69506d42b014db0eeb884ec380e9
Eric Ricketts <eric_ricketts@me.com>
2025-10-25 14:08:41 -0400

    assert on output

--- SectionSix/src/test/java/org/example/AgeCalculatorTest.java
+++ SectionSix/src/test/java/org/example/AgeCalculatorTest.java
@@ -5,10 +5,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.Scanner;
 
+/*
+    Stackoverflow article on how to read from System.out
+    https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
+*/
 public class AgeCalculatorTest {
+    public static final String NAME = "Name: ";
+
+    public static String readName() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        return NAME.concat(input);
+    }
+
+    public void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
 
     String expected, result;
     Integer resultInt;
@@ -16,6 +32,7 @@ public class AgeCalculatorTest {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final InputStream originalIn = System.in;
     private final PrintStream originalOut = System.out;
     private final PrintStream originalErr = System.err;
 
@@ -27,6 +44,7 @@ public class AgeCalculatorTest {
 
     @AfterEach
     public void restoreStreams() {
+        System.setIn(originalIn);
         System.setOut(originalOut);
         System.setErr(originalErr);
     }
@@ -51,7 +69,7 @@ public class AgeCalculatorTest {
     public void testStackOverflowCode() {
         // below is a test taken from Stack Overflow article
         // Test below extends this concept to the class and methods I created
-         expected = "hello";
+        expected = "hello";
         System.out.print(expected);
         result = outContent.toString();
         Assertions.assertEquals(expected, result);
@@ -74,6 +92,22 @@ public class AgeCalculatorTest {
         resultDouble = AgeCalculator.getAgeDouble("2000", "1989.5");
         result = String.valueOf(resultDouble);
 
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testAssertOnInput() {
+        expected = "Baeldung";
+        provideInput("Baeldung");
+        String result = readName();
+        Assertions.assertEquals(NAME.concat("Baeldung"), result);
+    }
+
+    @Test
+    public void testAssertOnOutput() {
+        expected = "Baeldung";
+        System.out.print("Baeldung");
+        result = outContent.toString();
         Assertions.assertEquals(expected, result);
     }
 }
