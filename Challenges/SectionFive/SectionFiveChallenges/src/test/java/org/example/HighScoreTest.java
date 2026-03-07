98e63d06349802cb9117cc34c1751b5166f4762f
EricRicketts <eric_ricketts@icloud.com>
2026-03-07 07:46:17 -0500

    update the first challenge in Section Five with additional test cases

--- Challenges/SectionFive/SectionFiveChallenges/src/test/java/org/example/HighScoreTest.java
+++ Challenges/SectionFive/SectionFiveChallenges/src/test/java/org/example/HighScoreTest.java
@@ -6,11 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HighScoreTest {
 
+    String[] names, expected;
+    String message;
+    int[] scores;
+    int position;
+
     @Test
     public void testHighScore() {
-        int[] scores = new int[] {1500, 1000, 500, 100, 25};
-        String[] names = new String[] {"Bert", "Bill", "Bob", "Buster", "Bo"};
-        String[] expected = new String[] {
+        scores = new int[] {1500, 1000, 500, 100, 25};
+        names = new String[] {"Bert", "Bill", "Bob", "Buster", "Bo"};
+        expected = new String[] {
             "Bert managed to get into position 1 on the high score list.",
             "Bill managed to get into position 1 on the high score list.",
             "Bob managed to get into position 2 on the high score list.",
@@ -18,8 +23,24 @@ public class HighScoreTest {
             "Bo managed to get into position 4 on the high score list."
         };
         for (int index = 0; index < scores.length; index += 1) {
-            int position = HighScore.calculateHighScorePosition(scores[index]);
-            String message = HighScore.displayHighScorePosition(names[index], position);
+            position = HighScore.calculateHighScorePosition(scores[index]);
+            message = HighScore.displayHighScorePosition(names[index], position);
+            assertEquals(expected[index], message);
+        }
+    }
+
+    @Test
+    public void testHighScoreWithOtherBoundaryConditions() {
+        scores = new int[] {999, 499, 99};
+        names = new String[] {"Charles", "Chuck", "Cody"};
+        expected = new String[] {
+            "Charles managed to get into position 2 on the high score list.",
+            "Chuck managed to get into position 3 on the high score list.",
+            "Cody managed to get into position 4 on the high score list."
+        };
+        for (int index = 0; index < scores.length; index += 1) {
+            position = HighScore.calculateHighScorePosition(scores[index]);
+            message = HighScore.displayHighScorePosition(names[index], position);
             assertEquals(expected[index], message);
         }
     }
