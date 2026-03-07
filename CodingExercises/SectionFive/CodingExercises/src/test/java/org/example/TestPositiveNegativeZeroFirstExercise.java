5f42847534593c5e0f3fe271f4ebd9e934459e73
EricRicketts <eric_ricketts@icloud.com>
2026-03-07 08:59:37 -0500

    rename the first coding exercise test for section five

--- CodingExercises/SectionFive/CodingExercises/src/test/java/org/example/TestPositiveNegativeZeroFirstExercise.java
+++ CodingExercises/SectionFive/CodingExercises/src/test/java/org/example/TestPositiveNegativeZeroFirstExercise.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestPositiveNegativeZero {
+public class TestPositiveNegativeZeroFirstExercise {
 
     @Test
     public void testPositiveNegativeZero() {
