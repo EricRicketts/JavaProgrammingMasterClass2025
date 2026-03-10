042f533504b7130f4190531e01971b1257351072
EricRicketts <eric_ricketts@icloud.com>
2026-03-10 10:44:32 -0400

    add day of week test with if else option

--- Challenges/SectionSix/src/main/java/org/example/DayOfWeek.java
+++ Challenges/SectionSix/src/main/java/org/example/DayOfWeek.java
@@ -35,4 +35,24 @@ public class DayOfWeek {
         };
         return dayOfWeek;
     }
+
+    public static String getDayOfWeekWithIfElse(int day) {
+        if (day == 0) {
+            return daysOfWeek[0];
+        } else if (day == 1) {
+            return daysOfWeek[1];
+        } else if (day == 2) {
+            return daysOfWeek[2];
+        } else if (day == 3) {
+            return daysOfWeek[3];
+        } else if (day == 4) {
+            return daysOfWeek[4];
+        } else if (day == 5) {
+            return daysOfWeek[5];
+        } else if (day == 6) {
+            return daysOfWeek[6];
+        } else {
+            return "Invalid Day";
+        }
+    }
 }
--- Challenges/SectionSix/src/test/java/org/example/DayOfWeekTest.java
+++ Challenges/SectionSix/src/test/java/org/example/DayOfWeekTest.java
@@ -6,15 +6,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DayOfWeekTest {
 
+    String[] expectedDays = {
+            "Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday", "Sunday", "Invalid Day"
+    };
     @Test
     public void testDayOfWeek() {
         for (int i = 0; i < 8; i++) {
-            String[] expectedDays = {
-                    "Monday", "Tuesday", "Wednesday", "Thursday",
-                    "Friday", "Saturday", "Sunday", "Invalid Day"
-            };
             String expectedDayOfWeek = expectedDays[i];
             String dayOfWeek = DayOfWeek.getDayOfWeek(i);
+            assertEquals(expectedDayOfWeek, dayOfWeek);
+        }
+    }
+
+    @Test
+    public void testDayOfWeekWithIfElse() {
+        for (int i = 0; i < 8; i++) {
+            String expectedDayOfWeek = expectedDays[i];
+            String dayOfWeek = DayOfWeek.getDayOfWeekWithIfElse(i);
             assertEquals(expectedDayOfWeek, dayOfWeek);
         }
     }
