1efe97e68b83a1e66abcb327eb07c666071e8bce
EricRicketts <eric_ricketts@icloud.com>
2026-03-10 09:40:17 -0400

    Section Six Nato Letter Challenge

---
+++ Challenges/SectionSix/src/main/java/org/example/NatoPhoneticAlphabet.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class NatoPhoneticAlphabet {
+    public static final Character[] LETTERS =
+            {       'A', 'B', 'C', 'D', 'E',
+                    'F', 'G', 'H', 'I', 'J',
+                    'K', 'L', 'M', 'N', 'O',
+                    'P', 'Q', 'R', 'S', 'T',
+                    'U', 'V', 'W', 'X', 'Y', 'Z'
+            };
+
+    public static final String[] NATO_LETTERS = {
+            "Able", "Baker", "Charlie", "Dog", "Easy",
+            "Fox", "George", "How", "Item", "Jig",
+            "King", "Love", "Mike", "Nan", "Oboe",
+            "Peter", "Queen", "Roger", "Sugar", "Tare",
+            "Uncle", "Victor", "William", "X-ray", "Yoke", "Zebra"
+    };
+public static String chooseNatoPhoneticLetterViaTraditionalSwitch(char letter) {
+
+    String natoLetter = "";
+    switch (letter) {
+        case 'A': case 'B': case 'C': case 'D': case 'E':
+        case 'F': case 'G': case 'H': case 'I': case 'J':
+        case 'K': case 'L': case 'M': case 'N': case 'O':
+        case 'P': case 'Q': case 'R': case 'S': case 'T':
+        case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+        {
+            int index = Arrays.asList(LETTERS).indexOf(letter);
+            natoLetter = NATO_LETTERS[index];
+            break;
+        }
+    }
+    return natoLetter;
+}
+
+}
---
+++ Challenges/SectionSix/src/test/java/org/example/NatoPhoneticAlphabetTraditionalSwitchTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.example.NatoPhoneticAlphabet.LETTERS;
+import static org.example.NatoPhoneticAlphabet.NATO_LETTERS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NatoPhoneticAlphabetTraditionalSwitchTest {
+
+    @Test
+    public void testChooseNatoPhoneticLetterViaTraditionalSwitch() {
+        for (int index = 0; index < 26; index++) {
+            char currentLetter = LETTERS[index];
+            String expectedNatoLetter = NATO_LETTERS[index];
+            String resultantNatoLetter =
+                    NatoPhoneticAlphabet.chooseNatoPhoneticLetterViaTraditionalSwitch(currentLetter);
+            assertEquals(expectedNatoLetter, resultantNatoLetter);
+        }
+    }
+
+}
