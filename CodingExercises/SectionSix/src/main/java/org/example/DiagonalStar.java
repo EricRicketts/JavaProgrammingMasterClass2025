61314a48969e08d1ae26facb0616a3ba3771ddcd
EricRicketts <eric_ricketts@icloud.com>
2026-03-16 16:29:33 -0400

    Section Six Coding Exercise 27 Diagonal Star Pattern

--- CodingExercises/SectionSix/src/main/java/org/example/DiagonalStar.java
+++ CodingExercises/SectionSix/src/main/java/org/example/DiagonalStar.java
@@ -10,12 +10,16 @@ public class DiagonalStar {
         for (int row = 1; row <= number; row++) {
             if (row == 1) {
                 starPattern.append("*".repeat(number)).append("\n");
-                continue;
+                continue; // this is needed because once we check for row == 1
+                // we need to return to the loop and increment the row variable
             }
             if (row == number) {
                 starPattern.append("*".repeat(number)).append("\n");
-                break;
+                break; // this is needed because we are at the end of the star pattern
+                // and we need to break out of the loop
             }
+            // the following code is for where we are not at the first or last rows
+            // of the star pattern but in between the first and last rows
             for (int col = 1; col <= number; col++) {
                 boolean appendStarCondition =
                     (col == 1) || (col == row) || ((col == (number - row) + 1));
--- CodingExercises/SectionSix/src/test/java/org/example/DiagonalStarTest.java
+++ CodingExercises/SectionSix/src/test/java/org/example/DiagonalStarTest.java
@@ -6,10 +6,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiagonalStarTest {
 
+    String expected, result;
+
     @Test
     public void testDiagonalStarNumberFive() {
-        String expected ="*****\n** **\n* * *\n** **\n*****\n";
-        String result = DiagonalStar.printSquareStar(5);
+        expected = "*****\n** **\n* * *\n** **\n*****\n";
+        result = DiagonalStar.printSquareStar(5);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDiagonalStarNumberSix() {
+        expected = "******\n**  **\n* ** *\n* ** *\n**  **\n******\n";
+        result = DiagonalStar.printSquareStar(6);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDiagonalStarNumberSeven() {
+        expected =
+                "*******\n**   **\n* * * *\n*  *  *\n* * * *\n**   **\n*******\n";
+        result = DiagonalStar.printSquareStar(7);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDiagonalStarNumberEight() {
+        expected =
+                "********\n**    **\n* *  * *\n*  **  *\n*  **  *\n* *  * *\n**    **\n********\n";
+        result = DiagonalStar.printSquareStar(8);
         assertEquals(expected, result);
     }
 }
