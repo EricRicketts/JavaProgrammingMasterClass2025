58d39fb1c0eb8a6099d869f7ea587c72c7da33c9
EricRicketts <eric_ricketts@icloud.com>
2026-03-20 07:40:22 -0400

    first test of Java POJOs, just get the Student instances created

---
+++ SectionSeven/src/test/java/org/example/StudentTest.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StudentTest {
+
+    ArrayList<Student> students = new ArrayList<Student>();
+
+    @BeforeEach
+    public void setUp() {
+        for (int i = 0; i < 5; i++) {
+            Student s = new Student("S92300" + i,
+                    switch (i) {
+                        case 0 -> "Mary";
+                        case 1 -> "Carol";
+                        case 2 -> "Tim";
+                        case 3 -> "Harry";
+                        case 4 -> "Lisa";
+                        default -> "Anonymous";
+                    },
+                    switch (i) {
+                        case 0 -> "1/1/1985";
+                        case 1 -> "2/1/1985";
+                        case 2 -> "3/1/1985";
+                        case 3 -> "4/1/1985";
+                        case 4 -> "5/1/1985";
+                        default -> "Default Birth Date";
+                    },
+                    "Java Masterclass"
+            );
+            students.add(s);
+        }
+    }
+
+    @Test
+    public void testStudentPOJOs() {
+        assertEquals(5, students.size());
+    }
+}
