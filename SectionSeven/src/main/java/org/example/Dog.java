246b5d4f0998ec6aef863dbe35cadd2968e25509
EricRicketts <eric_ricketts@icloud.com>
2026-03-22 05:24:56 -0400

    add more comments on inheritance

--- SectionSeven/src/main/java/org/example/Dog.java
+++ SectionSeven/src/main/java/org/example/Dog.java
@@ -9,34 +9,49 @@ public class Dog extends Animal {
         takes place, the subclass (inheriting class) gets all the attributes (fields) as long
         as those attributes and methods are not private.  Private fields and methods are not
         inherited from a parent class to a child class.
+
+        One important note about inheritance in Java, a child class can only inherit from
+        one parent class.  Java does not allow a child class to inherit from multiple parent
+        classes.
     */
 
     private String earShape, tailShape;
 
     public Dog() {
-        // This is the no-arguments constructor for the Dog class.  It makes a call to the
-        // three-argument constructor in the Animal class.  If one does not make a call to
-        // "super" in the constructor, then Java makes an implicit call to the super class
-        // default constructor.  If your super class does not have a default constructor, then
-        // you must explicitly call "super" in all of your child class constructors passing
-        // in the correct arguments for each call.
+        /*
+            This is the no-arguments constructor for the Dog class.  It makes a call to the
+            three-argument constructor in the Animal class.  If one does not make a call to
+            "super" in the constructor, then Java makes an implicit call to the super class
+            default constructor.  If your super class does not have a default constructor, then
+            you must explicitly call "super" in all of your child class constructors passing
+            in the correct arguments for each call.
+         */
         super("Mutt", "Big", 50);
     }
 
     public Dog(String type, double weight) {
-        // The keyword "this" enables us to call the four-argument constructor
-        // defined immediately below.
+        /*
+            The keyword "this" enables us to call the four-argument constructor
+            defined immediately below.
+         */
         this(type, weight, "Perky", "Curled");
     }
 
     public Dog(String type, double weight, String earShape, String tailShape) {
-        // A call is made to the super constructor (Animal) using an expression instead
-        // of a passed-in parameter to set the weight of the dog.  The "super" keyword,
-        // much like the "this" keyword, must be first in the constructor call.  Though
-        // the "super" keyword refers to the three-argument constructor in the Animal
-        //  class, the call to the "super" constructor is made within the context of
-        // the Dog class.  Thus, at the end of this constructor all, an instance of the
-        // Dog class is created.
+        /*
+            A call is made to the super constructor (Animal) using an expression instead
+            of a passed-in parameter to set the weight of the dog.  The "super" keyword,
+            much like the "this" keyword, must be first in the constructor call.  Though
+            the "super" keyword refers to the three-argument constructor in the Animal
+            class, the call to the "super" constructor is made within the context of
+            the Dog class.  Thus, at the end of this constructor all, an instance of the
+            Dog class is created.  This call to "super" is made to set the inherited fields
+            of the parent class (Animal) within the child class (Dog).  Because the Dog
+            inherits from Animal, there is no need to declare the attributes of Animal
+            in the Dog class.  This happens because the process of inheritance implicitly
+            instantiates all non-private attributes and methods of the parent class in
+            the child class.
+         */
         super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight);
         this.earShape = earShape;
         this.tailShape = tailShape;
@@ -46,9 +61,11 @@ public class Dog extends Animal {
         Another important note here, all subclasses can execute the methods of the parent
         class even though the code itself is declared in the parent class.  Below we
         have the move() and makeNoise() methods from the parent class.  However, they
-        are overridden in the child class to add additional functionality.  An override
-        occurs when you define a method in the subclass which as the same signature as
-        the parent class method.
+        are overridden in the child class to add functionality considered unique to the
+        child class.  An override occurs when you define a method in the subclass which
+        has the same signature as the parent class method.  A method signature is the
+        method name and parameter list.  The parameter list is defined as the order, number
+        and type of parameters.
     */
     @Override
     public String move(String speed) {
@@ -74,7 +91,7 @@ public class Dog extends Animal {
     }
 
     /*
-        Four methods below are unique to dog behavior.
+        Four methods below are unique to dog behavior, they are not in the parent class.
     */
     private String bark() {
         return "Woof!";
@@ -101,8 +118,10 @@ public class Dog extends Animal {
             methods in child class methods, which means we can call parent class
             methods in the child context.  In this case since the parent class
             toString() method captures all the information we need about the
-            parent class attributes, there is no need to define a method in the
-            child class to return a string containing the parent class attributes.
+            parent class attributes.  There is no need to define a method in the
+            child class to return a string containing the parent class attributes,
+            as the "super" keyword allows us to call the parent toString() method
+            and obtain the parent class attributes.
         */
         return "Dog{" +
                 "earShape='" + earShape + '\'' +
