039211e6b07153ed00efc49b815fce987c82fd9e
EricRicketts <eric_ricketts@icloud.com>
2026-03-23 09:39:42 -0400

    Common String Comparison methods

--- Challenges/SectionSeven/src/test/java/org/example/StringMethods.java
+++ Challenges/SectionSeven/src/test/java/org/example/StringMethods.java
@@ -22,9 +22,24 @@ public class StringMethods {
         // so the last index is 75, so one must start counting backwards from 75.
         assertEquals(76, sampleString.length());
         assertEquals( 'd', sampleString.charAt(37));
-        assertEquals(19, sampleString.indexOf("r"));
-        assertEquals(65, sampleString.lastIndexOf("p"));
+        assertEquals(19, sampleString.indexOf('r'));
+        assertEquals(65, sampleString.lastIndexOf('p'));
+        // below we find the second 'p' position starting from index 19
+        assertEquals(24, sampleString.indexOf('p', 19));
+        // starts and index 56 and then works backwards to find the first occurrence of the letter.
+        assertEquals(39, sampleString.lastIndexOf('n', 56));
+        assertEquals(46, sampleString.lastIndexOf('i', 56));
         assertTrue(emptyString.isEmpty());
         assertTrue(blankString.isBlank());
+    }
+
+    @Test
+    public void testStringComparisonMethods() {
+        sampleString = "This IS a sample Text.";
+        assertEquals("this is a sample text.", sampleString.toLowerCase());
+        assertTrue("This IS a sample Text.".equals(sampleString));
+        assertTrue(sampleString.startsWith("Thi"));
+        assertTrue(sampleString.endsWith("xt."));
+        assertTrue(sampleString.contains("a s"));
     }
 }
