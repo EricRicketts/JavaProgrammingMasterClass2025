700bd3bffc9ab018cf5a36ee2ae34c4e061a0157
EricRicketts <eric_ricketts@icloud.com>
2026-03-22 20:45:52 -0400

    Test two different strings one with tabs and one with spaces

--- Challenges/SectionSeven/src/test/java/org/example/TextBlockTest.java
+++ Challenges/SectionSeven/src/test/java/org/example/TextBlockTest.java
@@ -1,9 +1,8 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TextBlockTest {
 
@@ -22,6 +21,18 @@ public class TextBlockTest {
 
     @Test
     public void testFormattedStringAndTextBlockAreEqual() {
-        assertEquals(formattedString, textBlock);
+        formattedString =
+                formattedString.replaceAll("\t", "    ").replace(" ", "·");
+        textBlock =
+                textBlock.replaceAll("\t", "    ").replace(" ", "·");
+        int max = Math.max(formattedString.length(), textBlock.length());
+        for (int i = 0; i < max; i++) {
+            char ca = (i < formattedString.length()) ? formattedString.charAt(i) : '\0';
+            char cb = (i < textBlock.length()) ? textBlock.charAt(i) : '\0';
+            String mismatchMessage =
+                    "Mismatch at index " + i + " a = [" + (int) ca + "], " +
+                            "b = [" + (int) cb + "]";
+            Assertions.assertEquals(ca, cb, mismatchMessage);
+        }
     }
 }
