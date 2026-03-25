26c3d558d476a363dc62ef1a1751fe3abbfd965b
EricRicketts <eric_ricketts@icloud.com>
2026-03-25 15:58:58 -0400

    added a test case for the default switch path

--- SectionEight/src/test/java/org/example/MovieFactoryTest.java
+++ SectionEight/src/test/java/org/example/MovieFactoryTest.java
@@ -14,7 +14,8 @@ public class MovieFactoryTest {
         expected = new String[]{
                 "The movie title is 'Raiders of the Lost Ark' and the movie type is Adventure",
                 "The movie title is 'The Naked Gun' and the movie type is Comedy",
-                "The movie title is 'Dune' and the movie type is ScienceFiction"
+                "The movie title is 'Dune' and the movie type is ScienceFiction",
+                "The movie title is 'Lincoln' and the movie type is Movie"
         };
     }
 
@@ -23,17 +24,20 @@ public class MovieFactoryTest {
         String[][] inputs = new String[][]{
                 {"Adventure", "Raiders of the Lost Ark"},
                 {"Comedy", "The Naked Gun"},
-                {"ScienceFiction", "Dune"}
+                {"ScienceFiction", "Dune"},
+                {"Documentary", "Lincoln"}
         };
         Movie[] movies = new Movie[]{
                 Movie.getMovie(inputs[0][0], inputs[0][1]),
                 Movie.getMovie(inputs[1][0], inputs[1][1]),
-                Movie.getMovie(inputs[2][0], inputs[2][1])
+                Movie.getMovie(inputs[2][0], inputs[2][1]),
+                Movie.getMovie(inputs[3][0], inputs[3][1])
         };
         results = new String[]{
                 movies[0].watchMovie().split("\n")[0],
                 movies[1].watchMovie().split("\n")[0],
                 movies[2].watchMovie().split("\n")[0],
+                movies[3].watchMovie().split("\n")[0],
         };
         assertArrayEquals(expected, results);
     }
