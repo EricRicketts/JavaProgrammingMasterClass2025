51999d3b70b4a3beb775ee2f993eb8ce202da8e2
EricRicketts <eric_ricketts@icloud.com>
2026-03-28 16:15:19 -0400

    Section Eight Lession 116 Code and Test Burger Class

--- Challenges/SectionEight/src/main/java/org/example/Burger.java
+++ Challenges/SectionEight/src/main/java/org/example/Burger.java
@@ -4,22 +4,28 @@ public class Burger {
 
     private final String type;
     private final double price;
-    String[] toppings = new String[3];
+    Topping[] toppings = new Topping[3];
 
     public Burger(String type, double price){
-        this.type = type;
-        if (type.equalsIgnoreCase("small")) {
-            this.price = 6.00;
-        } else if (type.equalsIgnoreCase("medium")) {
-            this.price = 15.00;
-        }
-        else if (type.equalsIgnoreCase("large")) {
-            this.price = 25.00;
-        } else {
-            this.price =  price;
+        Topping[] toppings = new Topping[3];
+        this.type = type.toLowerCase();
+        switch(type) {
+            case "small" -> this.price = 6.00;
+            case "medium" -> this.price = 15.00;
+            case "large" -> this.price = 25.00;
+            default -> this.price = price;
         }
     }
 
+    public void setTopping(String type) {
+        for (int i = 0; i < this.getToppings().length; i++) {
+            if (this.getToppings()[i] == null) {
+                Topping topping = new Topping(type);
+                this.getToppings()[i] = topping;
+                break;
+            }
+        }
+    }
     public String getType() {
         return type;
     }
@@ -28,7 +34,7 @@ public class Burger {
         return price;
     }
 
-    public String[] getToppings() {
+    public Topping[] getToppings() {
         return toppings;
     }
 }
--- Challenges/SectionEight/src/test/java/org/example/BurgerTest.java
+++ Challenges/SectionEight/src/test/java/org/example/BurgerTest.java
@@ -1,13 +1,50 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BurgerTest {
 
-    Burger burger, smallBurger, largeBurger;
+    Burger smallBurger, mediumBurger, largeBurger, burger;
+    String[] expectedTypes, resultantTypes;
+    double[] expectedPrices, resultantPrices;
 
     @BeforeEach
     public void setUp() {
         smallBurger = new Burger("small", 15.00);
+        mediumBurger = new Burger("medium", 15.00);
+        largeBurger = new Burger("large", 15.00);
+        burger = new Burger("regular", 13.00);
     }
+
+    @Test
+    public void testAllBurgers() {
+        expectedTypes = new String[]{"small", "medium", "large", "regular"};
+        expectedPrices = new double[]{6.00, 15.00, 25.00, 12.00};
+        for (int i = 0; i < expectedTypes.length; i++) {
+            double priceSlot = 12.00;
+            String type = expectedTypes[i];
+            Burger burger = new Burger(type, priceSlot);
+            double expectedPrice = expectedPrices[i];
+            assertEquals(type, burger.getType());
+            assertEquals(expectedPrice, burger.getPrice());
+        }
+    }
+
+    @Test
+    public void testSetToppingOnABurger() {
+        String[] toppingTypes = new String[]{"lettuce", "tomatoes", "cheese"};
+        double[] expectedPrices = new double[]{1.00, 1.50, 2.00};
+        for (int i = 0; i < 3; i++) {
+            largeBurger.setTopping(toppingTypes[i]);
+        }
+        for (int i = 0; i < 3; i++) {
+            Topping topping = largeBurger.getToppings()[i];
+            assertEquals(toppingTypes[i], topping.getType());
+            assertEquals(expectedPrices[i], topping.getPrice());
+        }
+    }
+
 }
