031a283bbffe849744057c9b4ad4b75a2d077d7a
EricRicketts <eric_ricketts@icloud.com>
2026-03-28 07:15:34 -0400

    Section Eight Coding Exercise 40 Code and Test Hybrid  Car subclass

--- Challenges/SectionEight/src/main/java/org/example/HybridCar.java
+++ Challenges/SectionEight/src/main/java/org/example/HybridCar.java
@@ -12,6 +12,22 @@ public class HybridCar extends Car {
         this.cylinders = AutoChecks.checkCylinders(cylinders);
     }
 
+    @Override
+    public String startEngine() {
+        return super.startEngine() + " the gas powered part makes noise but the battery is silent.";
+    }
+
+    @Override
+    public String drive() {
+        return super.drive() +
+                " I can get long range with this car because of the extra power provided by the battery.";
+    }
+
+    @Override
+    protected String runEngine() {
+        return super.runEngine() + " when on battery only mode the card makes no noise.";
+    }
+
     public double getAvgKmPerLitre() {
         return avgKmPerLitre;
     }
--- Challenges/SectionEight/src/test/java/org/example/HybridCarTest.java
+++ Challenges/SectionEight/src/test/java/org/example/HybridCarTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HybridCarTest {
 
     HybridCar hybridCar;
+    String expected, result;
 
     @BeforeEach
     public void setUp() {
@@ -18,5 +19,33 @@ public class HybridCarTest {
     public void testSetAvgKmPerLitre() {
         hybridCar.setAvgKmPerLitre(-1);
         assertEquals(20.00, Scale.setScale(hybridCar.getAvgKmPerLitre(), 2));
+    }
+
+    @Test
+    public void testSetCylinders() {
+        hybridCar.setCylinders(-1);
+        assertEquals(4, hybridCar.getCylinders());
+    }
+
+    @Test
+    public void testStartEngine() {
+        expected = "Starting the engine on a/an HybridCar, the gas powered part makes noise but the battery is silent.";
+        result = hybridCar.startEngine();
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDrive() {
+        expected = "Driving a/an HybridCar, I can get long range " +
+                "with this car because of the extra power provided by the battery.";
+        result = hybridCar.drive();
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testRunEngine() {
+        expected = "The engine of a/an HybridCar is running, when on battery only mode the card makes no noise.";
+        result = hybridCar.runEngine();
+        assertEquals(expected, result);
     }
 }
