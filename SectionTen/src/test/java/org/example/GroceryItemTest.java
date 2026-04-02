3e7bbabcd5fef11b71b1cb9714fdf84dae0584f3
EricRicketts <eric_ricketts@icloud.com>
2026-04-02 08:39:23 -0400

    Add item to a list and then access items of the record in the list of records

--- SectionTen/src/test/java/org/example/GroceryItemTest.java
+++ SectionTen/src/test/java/org/example/GroceryItemTest.java
@@ -3,17 +3,28 @@ package org.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class GroceryItemTest {
 
     GroceryItem[] groceryList = new GroceryItem[3];
+    /*
+        Below, we instantiate an Array List of type GroceryItem.  This means all
+        elements in the list must be a GroceryItem.  We preserve Java's type
+        checking when we do this.  Note to complete the instantiation, we do
+        not need to specify the type in the second <>.
+     */
+    ArrayList<GroceryItem> secondGroceryList = new ArrayList<>();
 
     @BeforeEach
     public void setUp() {
         groceryList[0] = new GroceryItem("milk");
         groceryList[1] = new GroceryItem("apples", "PRODUCE", 6);
         groceryList[2] = new GroceryItem("oranges", "PRODUCE", 5);
+        secondGroceryList.add(new GroceryItem("butter"));
+        secondGroceryList.add(new GroceryItem("pears", "PRODUCE", 3));
     }
 
     @Test
