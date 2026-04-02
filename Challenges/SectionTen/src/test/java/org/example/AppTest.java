7d1c9f82ee7e0976fb527bc300827dcf2b29702b
EricRicketts <eric_ricketts@icloud.com>
2026-04-02 19:16:31 -0400

    set up challenges for section ten challenges

---
+++ Challenges/SectionTen/src/test/java/org/example/AppTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for the simple App.
+ */
+public class AppTest {
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
