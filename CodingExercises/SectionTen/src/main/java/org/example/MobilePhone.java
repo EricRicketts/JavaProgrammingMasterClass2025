9c710b6b8040bad06b4d4f1107876dc4a4a11da2
EricRicketts <eric_ricketts@icloud.com>
2026-04-05 20:11:28 -0400

    addNewContact to mobile phone

--- CodingExercises/SectionTen/src/main/java/org/example/MobilePhone.java
+++ CodingExercises/SectionTen/src/main/java/org/example/MobilePhone.java
@@ -5,4 +5,13 @@ import java.util.ArrayList;
 public class MobilePhone {
     private String myNumber;
     ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+        return !this.myContacts.contains(contact);
+    }
 }
---
+++ CodingExercises/SectionTen/src/test/java/org/example/MobilePhoneTest.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MobilePhoneTest {
+
+    private MobilePhone mobilePhone;
+    private Contact contact;
+
+    @BeforeEach
+    public void setUp() {
+        mobilePhone = new MobilePhone("0123456789");
+        Contact firstContact = new Contact("Elmer Fudd", "1122334455");
+        Contact secondContact = new Contact("Daffy Duck", "0011223344");
+        Contact thirdContact = new Contact("Bugs Bunny", "6677889900");
+        Contact fourthContact = new Contact("Foghorn Leghorn", "0123456789");
+        Contact fifthContact = new Contact("Porky Pig", "9876543210");
+        ArrayList<Contact> contacts = new ArrayList<>(List.of(
+                firstContact, secondContact, thirdContact, fourthContact, fifthContact
+        ));
+        for (Contact  contact : contacts) {
+            mobilePhone.myContacts = new ArrayList<>(
+                List.of(firstContact, secondContact, thirdContact, fourthContact, fifthContact)
+            );
+        }
+    }
+
+    @Test
+    public void testAddNewContactFalse() {
+        // The contact exists, so there is no need to add a contact
+        contact = new Contact("Elmer Fudd", "1122334455");
+        assertFalse(mobilePhone.addNewContact(contact));
+    }
+
+    @Test
+    public void testAddNewContactTrue() {
+        // The contact does not exist, so return true
+        contact = new Contact("John Doe", "2309458761");
+        assertTrue(mobilePhone.addNewContact(contact));
+    }
+}
