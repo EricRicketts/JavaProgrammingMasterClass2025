a8c66d59b78680e21dec1bf2853a3bc152fc942f
EricRicketts <eric_ricketts@icloud.com>
2026-04-06 15:57:11 -0400

    Primarily examples of Queue language to add items to a list, one example of stack syntax

--- SectionTen/src/test/java/org/example/LinkedListAddAndRemoveElementsTest.java
+++ SectionTen/src/test/java/org/example/LinkedListAddAndRemoveElementsTest.java
@@ -10,12 +10,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LinkedListAddAndRemoveElementsTest {
 
+    private static void addMoreElements(LinkedList<String> list) {
+        list.addFirst("Darwin"); // Add to the first position of the list.
+        list.addLast("Hobart");  // Add to the last position of the list.
+        // Queue methods
+        list.offer("Melbourne"); // Offer means to add to the rear of the list.
+        list.offerFirst("Brisbane"); // Another way of saying insert at the beginning of the list.
+        list.offerLast("Toowoomba"); // Another way of saying insert at the end of the list.
+        // Stack methods
+        list.push("Alice Springs"); // Stack language for adding to the beginning of the list.
+    }
     LinkedList<String> placesToVisit;
     String expected, result;
 
     @BeforeEach
     public void setUp() {
-        placesToVisit = new LinkedList<String>();
+        placesToVisit = new LinkedList<>();
     }
 
     @Test
@@ -27,9 +37,14 @@ public class LinkedListAddAndRemoveElementsTest {
 
     @Test
     public void testAddItemsUsingArrayListMethods() {
+        String expected = "[Alice Springs, Brisbane, Darwin, London, Paris, Berlin," +
+                " Hobart, Melbourne, Toowoomba]";
+        // Adding to a list adds to the back of the list unless specified otherwise
         placesToVisit.add("Paris");
-        placesToVisit.add("London");
+        placesToVisit.addFirst("London");
         placesToVisit.add("Berlin");
-
+        addMoreElements(placesToVisit);
+        String result = placesToVisit.toString();
+        assertEquals(expected, result);
     }
 }
