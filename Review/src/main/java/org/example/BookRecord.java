31ced9c1dde03b0ce06d831cb76b6e410073cd50
EricRicketts <eric_ricketts@icloud.com>
2026-04-11 14:31:32 -0400

    Review Exercises BookRecord test for null assignment to author

--- Review/src/main/java/org/example/BookRecord.java
+++ Review/src/main/java/org/example/BookRecord.java
@@ -6,10 +6,13 @@ public record BookRecord(String title, String author, int pages) {
 
     public BookRecord(String title, String author, int pages) {
         this.title = this.validateTitleAndReturn(title);
-        this.author = author;
+        this.author = this.validateAuthorAndReturn(author);
         this.pages = pages;
     }
 
+    private String validateAuthorAndReturn(String author) {
+        return Objects.requireNonNull(author, "Null value assigned to author");
+    }
     private String validateTitleAndReturn(String title) {
         return Objects.requireNonNull(title, "Null value assigned to title");
     }
--- Review/src/test/java/org/example/BookRecordTest.java
+++ Review/src/test/java/org/example/BookRecordTest.java
@@ -10,17 +10,28 @@ public class BookRecordTest {
 
     private BookRecord bookRecord;
 
+    private NullPointerException thrown;
+
     @BeforeEach
     public void setUp() {
         bookRecord = new BookRecord("Oliver Twist", "Charles Dickens", 608);
     }
 
     @Test
-    public void testNullValueForTitleInConstructor() {
-        NullPointerException thrown = assertThrows(
+    public void testBookRecordNullValueForTitleInConstructor() {
+        thrown = assertThrows(
                 NullPointerException.class,
                 () -> new BookRecord(null, "Charles Dickens", 608)
         );
         assertEquals("Null value assigned to title", thrown.getMessage());
+    }
+
+    @Test
+    public void testBookRecordNullValueForAuthorInConstructor() {
+        thrown = assertThrows(
+                NullPointerException.class,
+                () -> new BookRecord("Oliver Twist", null, 608)
+        );
+        assertEquals("Null value assigned to author", thrown.getMessage());
     }
 }
