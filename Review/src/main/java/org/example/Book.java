48671bf7993bbeb33d0a2ecff4522054d8a7a6a6
EricRicketts <eric_ricketts@icloud.com>
2026-04-11 11:02:02 -0400

    Rename validation helper methods

--- Review/src/main/java/org/example/Book.java
+++ Review/src/main/java/org/example/Book.java
@@ -6,11 +6,11 @@ public class Book {
     private final int pages;
 
     public Book(String title, String author, int pages) {
-        this.nullValueCheck(title);
+        this.validateNotNull(title);
         this.title = title;
-        this.nullValueCheck(author);
+        this.validateNotNull(author);
         this.author = author;
-        this.lessThanZeroPagesCheck(pages);
+        this.validatePages(pages);
         this.pages = pages;
     }
 
@@ -38,13 +38,13 @@ public class Book {
                 "Number of pages = " + this.getPages();
     }
 
-    private void nullValueCheck(String authorOrTitle) {
+    private void validateNotNull(String authorOrTitle) {
         if (authorOrTitle == null) {
             throw new NullPointerException("Null value assigned to author or title");
         }
     }
 
-    private void lessThanZeroPagesCheck(int pages) {
+    private void validatePages(int pages) {
         if (pages < 0) {
             throw new IllegalArgumentException("Number of pages assigned to book is less than zero");
         }
