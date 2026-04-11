a36bd5a57b525d2ff5501fde1a6ae8279472e5a5
EricRicketts <eric_ricketts@icloud.com>
2026-04-11 14:40:57 -0400

    Review exercises BookRecord test for negative number of pages in book

--- Review/src/main/java/org/example/BookRecord.java
+++ Review/src/main/java/org/example/BookRecord.java
@@ -7,11 +7,18 @@ public record BookRecord(String title, String author, int pages) {
     public BookRecord(String title, String author, int pages) {
         this.title = this.validateTitleAndReturn(title);
         this.author = this.validateAuthorAndReturn(author);
-        this.pages = pages;
+        this.pages = this.validatePagesAndReturn(pages);
     }
 
     private String validateAuthorAndReturn(String author) {
         return Objects.requireNonNull(author, "Null value assigned to author");
+    }
+
+    private int validatePagesAndReturn(int pages) {
+        if (pages < 0) {
+            throw new IllegalArgumentException("Number of pages assigned to book is less than zero");
+        }
+        return pages;
     }
     private String validateTitleAndReturn(String title) {
         return Objects.requireNonNull(title, "Null value assigned to title");
--- Review/src/test/java/org/example/BookRecordTest.java
+++ Review/src/test/java/org/example/BookRecordTest.java
@@ -18,20 +18,29 @@ public class BookRecordTest {
     }
 
     @Test
-    public void testBookRecordNullValueForTitleInConstructor() {
-        thrown = assertThrows(
-                NullPointerException.class,
-                () -> new BookRecord(null, "Charles Dickens", 608)
-        );
-        assertEquals("Null value assigned to title", thrown.getMessage());
-    }
-
-    @Test
     public void testBookRecordNullValueForAuthorInConstructor() {
         thrown = assertThrows(
                 NullPointerException.class,
                 () -> new BookRecord("Oliver Twist", null, 608)
         );
         assertEquals("Null value assigned to author", thrown.getMessage());
+    }
+
+    @Test
+    public void testBookRecordNegativeEntryForPagesInConstructor() {
+        IllegalArgumentException thrown = assertThrows(
+                IllegalArgumentException.class,
+                () -> new BookRecord("Oliver Twist", "Charles Dickens", -1)
+        );
+        assertEquals("Number of pages assigned to book is less than zero", thrown.getMessage());
+    }
+
+    @Test
+    public void testBookRecordNullValueForTitleInConstructor() {
+        thrown = assertThrows(
+                NullPointerException.class,
+                () -> new BookRecord(null, "Charles Dickens", 608)
+        );
+        assertEquals("Null value assigned to title", thrown.getMessage());
     }
 }
