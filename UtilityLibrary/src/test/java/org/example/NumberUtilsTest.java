0ae7dfdde76c21f437ee381bd58128f1fa67aa90
EricRicketts <eric_ricketts@icloud.com>
2026-04-13 16:51:53 -0400

    Feedback from AI, final tweek to the NumberUtils class

--- UtilityLibrary/src/test/java/org/example/NumberUtilsTest.java
+++ UtilityLibrary/src/test/java/org/example/NumberUtilsTest.java
@@ -12,13 +12,13 @@ public class NumberUtilsTest {
     public void testSetScaleLower() {
         BigDecimal expected = BigDecimal.valueOf(35.12);
         BigDecimal actual = NumberUtils.setScale(BigDecimal.valueOf(35.1243), 2);
-        assertEquals(0, expected.compareTo(actual), 0.01);
+        assertEquals(0, expected.compareTo(actual));
     }
 
     @Test
     public void testSetScaleHigher() {
         BigDecimal expected = BigDecimal.valueOf(35.13);
         BigDecimal actual = NumberUtils.setScale(BigDecimal.valueOf(35.1254), 2);
-        assertEquals(0, expected.compareTo(actual), 0.01);
+        assertEquals(0, expected.compareTo(actual));
     }
 }
