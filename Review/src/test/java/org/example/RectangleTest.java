a79ffd35e3e57fd386ff9736a728ba794c02e5b9
EricRicketts <eric_ricketts@icloud.com>
2026-04-21 09:44:16 -0400

    Reorganize the Rectangle class test code to nest Rectangle setter tests

--- Review/src/test/java/org/example/RectangleTest.java
+++ Review/src/test/java/org/example/RectangleTest.java
@@ -122,60 +122,64 @@ public class RectangleTest {
         public void testGetRectangleLength() {
             assertEquals(BigDecimal.valueOf(31.52), rectangle.getLength());
         }
-    
+
         @Test
         public void testGetRectangleWidth() {
             assertEquals(BigDecimal.valueOf(15.78), rectangle.getWidth());
         }
     }
 
-    @Test
-    public void testSetRectangleLengthNullValue() {
-        assertEquals(
-                "Null value not allowed for length",
-                assertThrows(
-                NullPointerException.class,
-                () -> rectangle.setLength(null)
-            ).getMessage()
-        );
-    }
+    @Nested
+    @DisplayName("Rectangle setter tests")
+    class RectangleSetterTests {
 
-    @Test
-    public void testSetRectangleLengthNegativeValue() {
-        assertEquals(
-                "Length is less than zero",
-                assertThrows(
-                        IllegalArgumentException.class,
-                        () -> rectangle.setLength(BigDecimal.valueOf(-10))
+        @Test
+        public void testSetRectangleLengthNullValue() {
+            assertEquals(
+                    "Null value not allowed for length",
+                    assertThrows(
+                    NullPointerException.class,
+                    () -> rectangle.setLength(null)
                 ).getMessage()
-        );
-    }
+            );
+        }
 
-    @Test
-    public void testSetRectangleWidthNullValue() {
-        assertEquals(
-                "Null value not allowed for width",
-                assertThrows(
-                        NullPointerException.class,
-                        () -> rectangle.setWidth(null)
-                ).getMessage()
-        );
-    }
+        @Test
+        public void testSetRectangleLengthNegativeValue() {
+            assertEquals(
+                    "Length is less than zero",
+                    assertThrows(
+                            IllegalArgumentException.class,
+                            () -> rectangle.setLength(BigDecimal.valueOf(-10))
+                    ).getMessage()
+            );
+        }
 
-    @Test
-    public void testSetRectangleWidthNegativeValue() {
-        assertEquals(
-                "Width is less than zero",
-                assertThrows(
-                        IllegalArgumentException.class,
-                        () -> rectangle.setWidth(BigDecimal.valueOf(-10))
-                ).getMessage()
-        );
-    }
+        @Test
+        public void testSetRectangleWidthNullValue() {
+            assertEquals(
+                    "Null value not allowed for width",
+                    assertThrows(
+                            NullPointerException.class,
+                            () -> rectangle.setWidth(null)
+                    ).getMessage()
+            );
+        }
 
-    @Test
-    public void testGetRectangleArea() {
-        assertEquals(BigDecimal.valueOf(497.39), rectangle.getArea());
-    }
+        @Test
+        public void testSetRectangleWidthNegativeValue() {
+            assertEquals(
+                    "Width is less than zero",
+                    assertThrows(
+                            IllegalArgumentException.class,
+                            () -> rectangle.setWidth(BigDecimal.valueOf(-10))
+                    ).getMessage()
+            );
+        }
 
+        @Test
+        public void testGetRectangleArea() {
+            assertEquals(BigDecimal.valueOf(497.39), rectangle.getArea());
+        }
+    }
 }
