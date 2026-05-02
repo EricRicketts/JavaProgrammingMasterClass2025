df1eb6efa76e24848ecd028b3112ca3ee130da85
EricRicketts <eric_ricketts@icloud.com>
2026-05-02 06:10:40 -0400

    Group scaling tests

--- UtilityLibrary/src/test/java/org/example/NumberUtilsTest.java
+++ UtilityLibrary/src/test/java/org/example/NumberUtilsTest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -8,17 +10,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NumberUtilsTest {
 
-    @Test
-    public void testSetScaleLower() {
-        BigDecimal expected = BigDecimal.valueOf(35.12);
-        BigDecimal actual = NumberUtils.setScale(BigDecimal.valueOf(35.1243), 2);
-        assertEquals(0, expected.compareTo(actual));
-    }
+    @Nested
+    @DisplayName("Scaling rounds correctly rounds down or up")
+    class SetScaleRounding {
 
-    @Test
-    public void testSetScaleHigher() {
-        BigDecimal expected = BigDecimal.valueOf(35.13);
-        BigDecimal actual = NumberUtils.setScale(BigDecimal.valueOf(35.1254), 2);
-        assertEquals(0, expected.compareTo(actual));
+        @Test
+        public void testSetScaleLower() {
+            BigDecimal expected = BigDecimal.valueOf(35.12);
+            BigDecimal actual = NumberUtils.setScale(BigDecimal.valueOf(35.1243), 2);
+            assertEquals(0, expected.compareTo(actual));
+        }
+
+        @Test
+        public void testSetScaleHigher() {
+            BigDecimal expected = BigDecimal.valueOf(35.13);
+            BigDecimal actual = NumberUtils.setScale(BigDecimal.valueOf(35.1254), 2);
+            assertEquals(0, expected.compareTo(actual));
+        }
     }
 }
