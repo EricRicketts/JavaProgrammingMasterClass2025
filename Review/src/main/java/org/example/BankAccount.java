12e998e583e411940418a7bd9f96e27800ace937
EricRicketts <eric_ricketts@icloud.com>
2026-05-08 04:42:23 -0400

    move validateAccountNumberAndReturn method into ValueValidator

--- Review/src/main/java/org/example/BankAccount.java
+++ Review/src/main/java/org/example/BankAccount.java
@@ -11,7 +11,7 @@ public class BankAccount {
 
     public BankAccount(String bankName, int accountNumber, BigDecimal balance) {
         this.bankName = ValueValidator.validateBankNameAndReturn(bankName);
-        this.accountNumber = validateAccountNumberAndReturn(accountNumber);
+        this.accountNumber = ValueValidator.validateAccountNumberAndReturn(accountNumber);
         BigDecimal nonNullOrNonNegativeBalance = validateBalanceAndReturn(balance);
         this.balance = NumberUtils.setScale(nonNullOrNonNegativeBalance, 2);
     }
@@ -54,27 +54,7 @@ public class BankAccount {
         );
     }
 
-   /*
-   private String validateBankNameAndReturn(String bankName) {
-        final Pattern ONE_OR_MORE_SPACES = Pattern.compile("\\s+");
-        final String EMPTY_STRING = "";
-
-        return switch(bankName) {
-            case null -> throw new NullPointerException(
-                    ErrorMessages.NULL_VALUE_MESSAGE_FOR_BANK_NAME.getErrorMessage()
-            );
-            case EMPTY_STRING -> throw new IllegalArgumentException(
-                    ErrorMessages.EMPTY_VALUE_MESSAGE_FOR_BANK_NAME.getErrorMessage()
-            );
-            case String s when ONE_OR_MORE_SPACES.matcher(s).matches() -> throw new IllegalArgumentException(
-                    ErrorMessages.BLANK_VALUE_MESSAGE_FOR_BANK_NAME.getErrorMessage()
-            );
-            default -> bankName;
-       };
-   }
-
-    */
-
+    /*
    private int validateAccountNumberAndReturn(int accountNumber) {
         if (accountNumber == 0) {
             throw new IllegalArgumentException(
@@ -88,6 +68,8 @@ public class BankAccount {
             return accountNumber;
         }
    }
+
+     */
 
    private BigDecimal validateBalanceAndReturn(BigDecimal balance) {
         BigDecimal nonNullBalance = ValueValidator.checkForNullValueAndReturn(
