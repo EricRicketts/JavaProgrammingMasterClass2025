032b73fbbdf0cbf0df2d6c4cf43e48a05efc998b
EricRicketts <eric_ricketts@icloud.com>
2026-05-08 07:20:52 -0400

    move the validation methods from the BookRecord record into the ValueValidator class

--- Review/src/main/java/org/example/BookRecord.java
+++ Review/src/main/java/org/example/BookRecord.java
@@ -6,9 +6,9 @@ import org.jetbrains.annotations.NotNull;
 public record BookRecord(String title, String author, int pages) {
 
     public BookRecord(String title, String author, int pages) {
-        this.title = validateTitleAndReturn(title);
-        this.author = validateAuthorAndReturn(author);
-        this.pages = validatePagesAndReturn(pages);
+        this.title = ValueValidator.validateTitleAndReturn(title);
+        this.author = ValueValidator.validateAuthorAndReturn(author);
+        this.pages = ValueValidator.validatePagesAndReturn(pages);
     }
 
     @Override
@@ -17,43 +17,9 @@ public record BookRecord(String title, String author, int pages) {
         return String.format(
                 "Title = %s%n" +
                 "Author = %s%n" +
-                "Number of pages = %s%n", title, author, pages).trim();
-    }
-
-    private String validateTitleAndReturn(String title) {
-        String nonNullTitle = ValueValidator.checkForNullValueAndReturn(
+                "Number of pages = %s%n",
                 title,
-                ErrorMessages.NULL_VALUE_MESSAGE_FOR_TITLE.getErrorMessage()
-        );
-        String nonEmptyTitle = ValueValidator.checkForEmptyValueAndReturn(
-                nonNullTitle,
-                ErrorMessages.EMPTY_VALUE_MESSAGE_FOR_TITLE.getErrorMessage()
-        );
-        return ValueValidator.checkForBlankValueAndReturn(
-                nonEmptyTitle,
-                ErrorMessages.BLANK_VALUE_MESSAGE_FOR_TITLE.getErrorMessage()
-        );
-    }
-
-    private String validateAuthorAndReturn(String author) {
-        String nonNullAuthor = ValueValidator.checkForNullValueAndReturn(
                 author,
-                ErrorMessages.NULL_VALUE_MESSAGE_FOR_AUTHOR.getErrorMessage()
-        );
-        String nonEmptyAuthor = ValueValidator.checkForEmptyValueAndReturn(
-                nonNullAuthor,
-                ErrorMessages.EMPTY_VALUE_MESSAGE_FOR_AUTHOR.getErrorMessage()
-        );
-        return ValueValidator.checkForBlankValueAndReturn(
-                nonEmptyAuthor,
-                ErrorMessages.BLANK_VALUE_MESSAGE_FOR_AUTHOR.getErrorMessage()
-        );
-    }
-
-    private int validatePagesAndReturn(int pages) {
-        return ValueValidator.checkForNegativeValueAndReturn(
-                pages,
-                ErrorMessages.NEGATIVE_VALUE_MESSAGE_FOR_PAGES.getErrorMessage()
-        );
+                pages).trim();
     }
 }
