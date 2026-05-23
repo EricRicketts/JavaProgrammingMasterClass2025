91c0f631c5ae5a4cd164283be0fc052bc892655b
EricRicketts <eric_ricketts@icloud.com>
2026-05-23 13:25:07 -0400

    add a comment on the choice of rounding

--- UtilityLibrary/src/main/java/org/example/NumberUtils.java
+++ UtilityLibrary/src/main/java/org/example/NumberUtils.java
@@ -5,6 +5,14 @@ import java.math.RoundingMode;
 
 public class NumberUtils {
 
+    /**
+     * setScale uses the RoundingMode.HALF_UP because this rounding mode rounds to the nearest
+     * numeric neighbor unless equidistant from both greater and lesser neighbors in which case
+     * it rounds up.  This is the accepted behavior for rounding taught in all US schools.
+     * @param value - value to be scaled
+     * @param scaleFactor - desired decimal precision
+     * @return scaled value at the required precision
+     */
     public static BigDecimal setScale(BigDecimal value, int scaleFactor) {
         return value.setScale(scaleFactor, RoundingMode.HALF_UP);
     }
