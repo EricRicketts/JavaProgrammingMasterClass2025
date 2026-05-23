b638024c5aea2a67653145f9aaba44889ba4ec4d
EricRicketts <eric_ricketts@icloud.com>
2026-05-23 19:31:11 -0400

    indentation adjustments

--- Review/src/test/java/org/example/TemperatureTest.java
+++ Review/src/test/java/org/example/TemperatureTest.java
@@ -231,7 +231,7 @@ public class TemperatureTest {
             Temperature temperature = new Temperature(new BigDecimal(temperatureValue), scaleFactor);
 
             assertEquals(new BigDecimal(result), temperature.getCelsius());
-        }
+       }
 
         @ParameterizedTest
         @CsvSource({"12.34565, 4, 12.3457", "12.34564, 4, 12.3456"})
@@ -251,7 +251,7 @@ public class TemperatureTest {
             assertThrows(
                 IllegalArgumentException.class,
                 () -> new Temperature(new BigDecimal("23.45"), -1)
-           );
+            );
         }
 
         @Test
