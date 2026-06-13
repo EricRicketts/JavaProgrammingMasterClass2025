2269dad161bff24a1c39f303fb62207c8b15e007
EricRicketts <eric_ricketts@icloud.com>
2026-06-13 05:42:12 -0400

    completed redesigned the Shape class due to an initial misunderstanding of the assignment

--- Review/src/main/java/org/example/Shape.java
+++ Review/src/main/java/org/example/Shape.java
@@ -1,37 +1,12 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 public class Shape {
 
-    private int length;
+    public Shape() {}
 
-    private int width;
-
-    public Shape(int length, int width) {
-        this.length = ValueValidator.validatePositiveIntAndReturn(length, "length");
-        this.width = ValueValidator.validatePositiveIntAndReturn(width, "width");
-    }
-
-    public Shape() {
-        this(1, 1);
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public void setLength(int length) {
-        this.length = ValueValidator.validatePositiveIntAndReturn(length, "length");
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = ValueValidator.validatePositiveIntAndReturn(width, "width");
-    }
-
-    public int area() {
-        return this.length * this.width;
+    public BigDecimal area() {
+        return new BigDecimal("1.5");
     }
 }
--- Review/src/test/java/org/example/ShapeTest.java
+++ Review/src/test/java/org/example/ShapeTest.java
@@ -1,79 +1,16 @@
 package org.example;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class ShapeTest {
 
-    private Shape firstShape;
-    private Shape secondShape;
-
-    @BeforeEach
-    public void setUp() {
-        firstShape = new Shape(5, 4);
-        secondShape = new Shape();
-    }
-
-    @Nested
-    @DisplayName("test shape constructors set fields")
-    class TestShapeConstructorsSetFields {
-
-        @Test
-        public void testTwoArgumentShapeConstructorSetsLength() {
-            assertEquals(5, firstShape.getLength());
-        }
-
-        @Test
-        public void testTwoArgumentShapeConstructorSetsWidth() {
-            assertEquals(4, firstShape.getWidth());
-        }
-
-        @Test
-        public void testNoArgumentShapeConstructorSetsLength() {
-            assertEquals(1, secondShape.getLength());
-        }
-
-        @Test
-        public void testNoArgumentShapeConstructorSetsWidth() {
-            assertEquals(1, secondShape.getWidth());
-        }
-    }
-
-    @Nested
-    @DisplayName("test shape constructor validation")
-    class TestShapeConstructorValidation {
-
-        @Test
-        public void testShapeConstructorRejectsNegativeLength() {
-            IllegalArgumentException exception =
-                assertThrows(
-                    IllegalArgumentException.class,
-                    () -> new Shape(-1, 1)
-            );
-
-            assertEquals(
-                ErrorMessages.negativeValue("length"),
-                exception.getMessage()
-            );
-        }
-
-        @Test
-        public void testShapeConstructorRejectsZeroLength() {
-            IllegalArgumentException exception =
-                assertThrows(
-                    IllegalArgumentException.class,
-                    () -> new Shape(0, 1)
-                );
-
-            assertEquals(
-                ErrorMessages.zeroValue("length"),
-                exception.getMessage()
-            );
-        }
+    @Test
+    public void testShapeArea() {
+        assertEquals(new BigDecimal("1.5"), new Shape().area());
     }
 }
