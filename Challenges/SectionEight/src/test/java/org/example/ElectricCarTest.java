5692b2cdb49d7516ef61a7a16c37c9c548767267
EricRicketts <eric_ricketts@icloud.com>
2026-06-25 10:55:27 -0400

    refactor electric car tests

--- Challenges/SectionEight/src/test/java/org/example/ElectricCarTest.java
+++ Challenges/SectionEight/src/test/java/org/example/ElectricCarTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -20,42 +22,62 @@ public class ElectricCarTest {
                 100);
     }
 
-    @Test
-    public void testStartEngine() {
-        expected = "Starting the engine on a/an ElectricCar, which is the future of all cars.";
-        result = electricCar.startEngine();
-        assertEquals(expected, result);
-    }
+    @Nested
+    @DisplayName("test electric car engine")
+    class TestElectricCarEngine {
 
-    @Test
-    public void testDrive() {
-        expected = "Driving a/an ElectricCar, which is quite a pleasant experience.";
-        result = electricCar.drive();
-        assertEquals(expected, result);
-    }
+        @Test
+        public void testStartEngine() {
+            expected = "Starting the engine on a/an ElectricCar, which is the future of all cars.";
+            result = electricCar.startEngine();
+            assertEquals(expected, result);
+        }
 
-    @Test
-    public void testRunEngine() {
-        expected = "The engine of a/an ElectricCar is running, " +
+        @Test
+        public void testRunEngine() {
+            expected = "The engine of a/an ElectricCar is running, " +
                 "which has no engine but electric motors.";
-        result = electricCar.runEngine();
-        assertEquals(expected, result);
+            result = electricCar.runEngine();
+            assertEquals(expected, result);
+        }
     }
 
-    @Test
-    public void testCheckAvgKmPerCharge() {
-        BigDecimal defaultAvgKmPerCharge = new BigDecimal("300.00");
-        electricCar.setAvgKmPerCharge(new BigDecimal("-10.00"));
-        BigDecimal avgKmPerCharge = electricCar.getAvgKmPerCharge();
-        avgKmPerCharge = NumberUtils.setScale(avgKmPerCharge, 2);
-        assertEquals(defaultAvgKmPerCharge, avgKmPerCharge);
+    @Nested
+    @DisplayName("test driving the electric car")
+    class TestDriveElectricCar {
+
+        @Test
+        public void testDrive() {
+            expected = "Driving a/an ElectricCar, which is quite a pleasant experience.";
+            result = electricCar.drive();
+            assertEquals(expected, result);
+        }
     }
 
-    @Test
-    public void testCheckBatterySize() {
-        int defaultBatterySize = 50;
-        electricCar.setBatterySize(-5);
-        int batterySize = electricCar.getBatterySize();
-        assertEquals(defaultBatterySize, batterySize);
+    @Nested
+    @DisplayName("test electric car average km per charge")
+    class TestElectricCarAverageKmPerCharge {
+
+        @Test
+        public void testCheckAvgKmPerCharge() {
+            BigDecimal defaultAvgKmPerCharge = new BigDecimal("300.00");
+            electricCar.setAvgKmPerCharge(new BigDecimal("-10.00"));
+            BigDecimal avgKmPerCharge = electricCar.getAvgKmPerCharge();
+            avgKmPerCharge = NumberUtils.setScale(avgKmPerCharge, 2);
+            assertEquals(defaultAvgKmPerCharge, avgKmPerCharge);
+        }
+    }
+
+    @Nested
+    @DisplayName("test electric car battery size")
+    class TestElectricCarBatterySize {
+
+        @Test
+        public void testCheckBatterySize() {
+            int defaultBatterySize = 50;
+            electricCar.setBatterySize(-5);
+            int batterySize = electricCar.getBatterySize();
+            assertEquals(defaultBatterySize, batterySize);
+        }
     }
 }
