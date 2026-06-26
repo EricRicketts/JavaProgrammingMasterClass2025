155573be0a598ec1d2fd7d0f7acffe97a24563ee
EricRicketts <eric_ricketts@icloud.com>
2026-06-26 16:11:12 -0400

    initialize arrays, get array length, query for value at index

--- SectionNine/src/test/java/org/example/FirstArrayTest.java
+++ SectionNine/src/test/java/org/example/FirstArrayTest.java
@@ -1,52 +1,88 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FirstArrayTest {
 
     private final int[] firstArray = new int[10];
-    private double[] secondArray;
+    private BigDecimal[] secondArray;
 
     private int expectedInt, resultantInt;
-    private double expectedDouble, resultantDouble;
+    private BigDecimal expectedBigDecimal, resultantBigDecimal;
 
     @BeforeEach
     public void setUp() {
         for(int i = 0; i < firstArray.length; i++) {
             firstArray[i] = 2*i + 1;
         }
-        secondArray = new double[]{0.12, 1.23, 2.45, 3.67, 4.89, 5.01};
+        secondArray = new BigDecimal[]{
+                new BigDecimal("0.12"),
+                new BigDecimal("1.23"),
+                new BigDecimal("2.45"),
+                new BigDecimal("3.67"),
+                new BigDecimal("4.89"),
+                new BigDecimal("5.01")
+        };
     }
 
-    @Test
-    public void testMemberFromFirstArray() {
-        expectedInt = 7;
-        resultantInt = firstArray[3];
-        assertEquals(expectedInt, resultantInt);
+    @Nested
+    @DisplayName("test declaring and initializing arrays")
+    class TestDeclaringAndInitializingArrays {
+
+        @Test
+        public void testDeclareAndInitializeArrayInOneStatement() {
+            int[] intArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            assertEquals(10, intArray.length);
+            assertEquals(5, intArray[5]);
+        }
+
+        @Test
+        public void testDeclareArrayAndInitializeAfterDeclaration() {
+            int[] intArray;
+            intArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            assertEquals(10, intArray.length);
+            assertEquals(7, intArray[7]);
+        }
     }
 
-    @Test
-    public void testLengthOfFirstArray() {
-        expectedInt = 10;
-        resultantInt = firstArray.length;
-        assertEquals(expectedInt, resultantInt);
+    @Nested
+    @DisplayName("test length and query of first array")
+    class TestLengthAndFirstArrayQuery {
+
+        @Test
+        public void testFirstArrayLength() {
+            assertEquals(10, firstArray.length);
+        }
+
+        @Test
+        public void testFirstArrayQuery() {
+            assertEquals(7, firstArray[3]);
+        }
     }
 
-    @Test
-    public void testNumberFromSecondArray() {
-        expectedDouble = 3.67;
-        resultantDouble = secondArray[3];
-        assertEquals(expectedDouble, resultantDouble);
-    }
+    @Nested
+    @DisplayName("test length and query of second array")
+    class TestLengthAndSecondArrayQuery {
 
-    @Test
-    public void testAnotherNumberFromSecondArray() {
-        expectedDouble = 5.01;
-        resultantDouble = secondArray[secondArray.length - 1];
-        assertEquals(expectedDouble, resultantDouble);
-    }
+        @Test
+        public void testSecondArrayLength() {
+            assertEquals(6, secondArray.length);
+        }
 
+        @Test
+        public void testNumberFromSecondArray() {
+            expectedBigDecimal = new BigDecimal("3.67");
+            resultantBigDecimal = secondArray[3];
+            assertEquals(expectedBigDecimal, resultantBigDecimal);
+        }
+    }
 }
