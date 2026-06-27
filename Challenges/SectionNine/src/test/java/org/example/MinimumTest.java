9d08249f2ec9971a13c82e506b734fcc8ed5440a
EricRicketts <eric_ricketts@icloud.com>
2026-06-27 15:17:28 -0400

    update tests for find min integer with ArrayWork readIntegers method

--- Challenges/SectionNine/src/test/java/org/example/MinimumTest.java
+++ Challenges/SectionNine/src/test/java/org/example/MinimumTest.java
@@ -48,26 +48,33 @@ public class MinimumTest {
         }
     }
 
-    @Test
-    public void testFirstSetOfIntegers() {
-        expected = 3;
-        result = ArrayWork.findMin(integers[0]);
-        assertEquals(expected, result);
-    }
+    @Nested
+    @DisplayName("test find minimum from set of integers")
+    class FindMinimumFromSetOfIntegers {
 
-    @Test
-    public void testSecondSetOfIntegers() {
-        expected = -1;
-        result = ArrayWork.findMin(integers[1]);
-        assertEquals(expected, result);
-    }
+        @Test
+        public void testFirstSetOfIntegers() {
+            expected = 3;
+            int[] input = ArrayWork.readIntegers(10, 100, 30, 50, 200, 3, 4, 5);
+            result = ArrayWork.findMin(input);
+            assertEquals(expected, result);
+        }
 
-    @Test
-    public void testRandomArray() {
-        int[] integers = ArrayWork.getRandomArray(100);
-        int minimum = ArrayWork.findMin(integers);
-        for (int currentInteger : integers) {
-            assertTrue(minimum <= currentInteger);
+        @Test
+        public void testSecondSetOfIntegers() {
+            expected = -1;
+            int[] input = ArrayWork.readIntegers(45, 25, 98, 0, -1, 32);
+            result = ArrayWork.findMin(input);
+            assertEquals(expected, result);
+        }
+
+        @Test
+        public void testRandomArray() {
+            int[] integers = ArrayWork.getRandomArray(100);
+            int minimum = ArrayWork.findMin(integers);
+            for (int currentInteger : integers) {
+                assertTrue(minimum <= currentInteger);
+            }
         }
     }
 }
