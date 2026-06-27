e6786bcb6af19cdb4304f68cb414d079cd696490
EricRicketts <eric_ricketts@icloud.com>
2026-06-27 14:33:14 -0400

    finish varargs

--- SectionNine/src/test/java/org/example/VarargsTest.java
+++ SectionNine/src/test/java/org/example/VarargsTest.java
@@ -1,12 +1,18 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VarargsTest {
-    String firstString, varargs, expected, result;
+    String firstString, expected, result;
     String[] stringArray;
 
     @BeforeEach
@@ -16,29 +22,34 @@ public class VarargsTest {
         stringArray = "Hello World Again".split(" ");
     }
 
-    @Test
-    public void testVarargsWithString() {
-        expected = "Hello World Again";
-        result = Varargs.printText(firstString);
-        assertEquals(expected, result);
-    }
+    @Nested
+    @DisplayName("test varargs options")
+    class TestVarArgsOptions {
 
-    @Test
-    public void testVarargsWithStringArray() {
-        result = Varargs.printText(stringArray);
-        assertEquals(expected, result);
-    }
+        @Test
+        public void testVarargsWithString() {
+            expected = "Hello World Again";
+            result = Varargs.printText(firstString);
+            assertEquals(expected, result);
+        }
 
-    @Test
-    public void testVarargsWithOneString() {
-        expected = "Hello";
-        result = Varargs.printText("Hello");
-        assertEquals(expected, result);
-    }
+        @Test
+        public void testVarargsWithStringArray() {
+            result = Varargs.printText(stringArray);
+            assertEquals(expected, result);
+        }
 
-    @Test
-    public void testVarargsWithAListOfStrings() {
-        result = Varargs.printText("Hello", "World", "Again");
-        assertEquals(expected, result);
+        @Test
+        public void testVarargsWithOneString() {
+            expected = "Hello";
+            result = Varargs.printText("Hello");
+            assertEquals(expected, result);
+        }
+
+        @Test
+        public void testVarargsWithAListOfStrings() {
+            result = Varargs.printText("Hello", "World", "Again");
+            assertEquals(expected, result);
+        }
     }
 }
