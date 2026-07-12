9f5eeca339aef794304cc2bc141184bd2a4f5856
EricRicketts <eric_ricketts@icloud.com>
2026-07-11 21:14:35 -0400

    add Cat and Lynx classes both extend Animal class

---
+++ Challenges/SectionEleven/src/main/java/org/example/Cat.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Cat extends Animal {
+
+    public Cat(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public String move(String speed) {
+        return this.getClass().getSimpleName() + " is walking at " + speed + " km/hr.";
+    }
+
+    @Override
+    public String makeNoise() {
+        if (Objects.equals(type, "Persian")) {
+            return "Loud Purr!";
+        } else {
+            return "Soft Purr";
+        }
+    }
+}
---
+++ Challenges/SectionEleven/src/main/java/org/example/Lynx.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Lynx extends Animal {
+
+    public Lynx(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public String move(String speed) {
+        return this.getClass().getSimpleName() + " is running at " + speed + " km/hr.";
+    }
+
+    @Override
+    public String makeNoise() {
+        if (Objects.equals(type, "Bobcat")) {
+            return "Loud Screech!";
+        } else {
+            return "Soft moan";
+        }
+    }
+}
