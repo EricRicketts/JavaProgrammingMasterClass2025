082fc0018a7ae689a0a77d9e5aa24cce7024c576
EricRicketts <eric_ricketts@icloud.com>
2026-07-14 09:39:44 -0400

    code and test ProductForSale setters

--- Challenges/SectionEleven/src/test/java/org/example/ProductForSaleTest.java
+++ Challenges/SectionEleven/src/test/java/org/example/ProductForSaleTest.java
@@ -15,7 +15,7 @@ public class ProductForSaleTest {
 
     @BeforeEach
     public void setUp() {
-        String description = "Battery powered edger for lawns.";
+        String description = "Battery powered Edger for lawns.";
         product = new ProductForSale("Edger", description, new BigDecimal("239.99"));
     }
 
@@ -30,12 +30,41 @@ public class ProductForSaleTest {
 
         @Test
         public void testProductForSaleGetDescription() {
-            assertEquals("Battery powered edger for lawns.", product.getDescription());
+            assertEquals("Battery powered Edger for lawns.", product.getDescription());
         }
 
         @Test
         public void testProductForSaleGetPrice() {
             assertEquals(new BigDecimal("239.99"), product.getPrice());
+        }
+    }
+
+    @Nested
+    @DisplayName("test ProductForSale setters")
+    class TestProductForSaleSetters {
+
+        @Test
+        public void testProductForSaleSetType() {
+            assertEquals("Edger", product.getType());
+            product.setType("Trimmer");
+
+            assertEquals("Trimmer", product.getType());
+        }
+
+        @Test
+        public void testProductForSaleSetDescription() {
+            assertEquals("Battery powered Edger for lawns.", product.getDescription());
+            product.setDescription("Battery powered Trimmer for lawns.");
+
+            assertEquals("Battery powered Trimmer for lawns.", product.getDescription());
+        }
+
+        @Test
+        public void testProductForSaleSetPrice() {
+            assertEquals(new BigDecimal("239.99"), product.getPrice());
+            product.setPrice(new BigDecimal("259.99"));
+
+            assertEquals(new BigDecimal("259.99"), product.getPrice());
         }
     }
 }
