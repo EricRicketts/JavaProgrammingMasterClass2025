6fcc4a2190099b3a0cd498775080ac13793945dc
EricRicketts <eric_ricketts@icloud.com>
2026-07-15 09:13:36 -0400

    add a product to the store

--- Challenges/SectionEleven/src/test/java/org/example/StoreTest.java
+++ Challenges/SectionEleven/src/test/java/org/example/StoreTest.java
@@ -1,10 +1,15 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StoreTest {
 
@@ -20,6 +25,23 @@ public class StoreTest {
         ArrayList<ProductForSale> productForSale = new ArrayList<>(List.of(edger));
         ArrayList<OrderItem> orders = new ArrayList<>();
         store = new Store(productForSale, orders);
+    }
+
+    @Nested
+    @DisplayName("test add product to store")
+    class TestAddProductToStore {
+
+        @Test
+        public void testAddProductToStore() {
+            assertEquals(1, store.getProductsForSale().size());
+            String description = "Battery powered Trimmer for lawn care";
+            ProductForSale trimmer =
+                new Trimmer("Trimmer", description, new BigDecimal("229.99"));
+
+            store.addProduct(trimmer);
+            assertEquals(2, store.getProductsForSale().size());
+        }
+
     }
 
 
