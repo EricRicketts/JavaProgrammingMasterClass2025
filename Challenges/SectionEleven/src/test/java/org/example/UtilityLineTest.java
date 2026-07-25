40e37ccf2e6eb476688d53feaad6e82866e51ffc
EricRicketts <eric_ricketts@icloud.com>
2026-07-25 15:17:42 -0400

    test different utilities to JSON

--- Challenges/SectionEleven/src/test/java/org/example/UtilityLineTest.java
+++ Challenges/SectionEleven/src/test/java/org/example/UtilityLineTest.java
@@ -14,7 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UtilityLineTest {
 
     List<Mappable> utilities = new ArrayList<>();
-    UtilityLine utility;
+    UtilityLine utility, fiberOptic, water, electrical;
+    String expected, result;
 
     @BeforeEach
     public void setUp() {
@@ -22,6 +23,9 @@ public class UtilityLineTest {
         utilities.add(new UtilityLine("Olympic Boulevard", UtilityType.WATER));
         utilities.add(new UtilityLine("Main Street", UtilityType.ELECTRICAL));
         utility = (UtilityLine) utilities.getFirst();
+        fiberOptic = (UtilityLine) utilities.getFirst();
+        water = (UtilityLine) utilities.get(1);
+        electrical = (UtilityLine) utilities.getLast();
     }
 
     @Nested
@@ -57,6 +61,50 @@ public class UtilityLineTest {
             utility.setType(UtilityType.GAS);
 
             assertEquals(GAS, utility.getType());
+        }
+    }
+
+    @Nested
+    @DisplayName("test different Utilities to JSON")
+    class TestDifferentUtilitiesToJSON {
+
+        String[] expectedJSON = new String[]{
+            """
+            "properties": {"type": "LINE", "label": "College Street (FIBER_OPTIC)",""" + " " +
+                """
+               "marker": "GREEN DOTTED", "name": "College Street", "utility": "FIBER_OPTIC"}""",
+            """
+            "properties": {"type": "LINE", "label": "Olympic Boulevard (WATER)",""" + " " +
+                """
+                "marker": "BLUE SOLID", "name": "Olympic Boulevard", "utility": "WATER"}""",
+            """
+            "properties": {"type": "LINE", "label": "Main Street (ELECTRICAL)",""" + " " +
+                """
+                "marker": "RED DASHED", "name": "Main Street", "utility": "ELECTRICAL"}"""
+        };
+
+        @Test
+        public void testFiberOpticUtilityToJSON() {
+            expected = expectedJSON[0];
+            result = Mappable.mapIt(fiberOptic);
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        public void testWaterUtilityToJSON() {
+            expected = expectedJSON[1];
+            result = Mappable.mapIt(water);
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        public void testElectricalUtilityToJSON() {
+            expected = expectedJSON[2];
+            result = Mappable.mapIt(electrical);
+
+            assertEquals(expected, result);
         }
     }
 }
