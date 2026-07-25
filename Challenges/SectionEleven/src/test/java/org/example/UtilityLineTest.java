e6f407e6dcf8a622e9388b1f203722a9f8518e5a
EricRicketts <eric_ricketts@icloud.com>
2026-07-25 12:58:22 -0400

    test getters for name and utility type

--- Challenges/SectionEleven/src/test/java/org/example/UtilityLineTest.java
+++ Challenges/SectionEleven/src/test/java/org/example/UtilityLineTest.java
@@ -1,9 +1,15 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.example.UtilityType.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UtilityLineTest {
 
@@ -12,11 +18,24 @@ public class UtilityLineTest {
 
     @BeforeEach
     public void setUp() {
-        utilities.add(new UtilityLine("College Street", UtilityType.FIBER_OPTIC));
+        utilities.add(new UtilityLine("College Street", FIBER_OPTIC));
         utilities.add(new UtilityLine("Olympic Boulevard", UtilityType.WATER));
         utilities.add(new UtilityLine("Main Street", UtilityType.ELECTRICAL));
         utility = (UtilityLine) utilities.getFirst();
     }
 
-    
+    @Nested
+    @DisplayName("test UtilityLine getters")
+    class TestUtilityLineGetters {
+
+        @Test
+        public void testUtilityLineGetName() {
+            assertEquals("College Street", utility.getName());
+        }
+
+        @Test
+        public void testUtilityLineGetType() {
+            assertEquals(FIBER_OPTIC, utility.getType());
+        }
+    }
 }
