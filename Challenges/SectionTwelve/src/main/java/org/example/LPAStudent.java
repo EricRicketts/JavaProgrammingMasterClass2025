a4bb53747a36470ad137a5efa653a3ad84381aa1
EricRicketts <eric_ricketts@icloud.com>
2026-08-05 11:26:49 -0400

    add another method to LPAStudent

--- Challenges/SectionTwelve/src/main/java/org/example/LPAStudent.java
+++ Challenges/SectionTwelve/src/main/java/org/example/LPAStudent.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
 import java.util.Random;
 
 public class LPAStudent extends ThirdStudent {
@@ -25,5 +26,20 @@ public class LPAStudent extends ThirdStudent {
     @Override
     public String toString() {
         return "%s %8.2f%%".formatted(super.toString(), this.getPercentComplete());
+    }
+
+    public <T extends ThirdStudent> String printList(List<T> students) {
+        // Above generic means I can pass any collection of ThirdStudent or LPAStudent.
+        return printMoreList(students);
+    }
+
+    public static String printMoreList(List<? extends ThirdStudent> students) {
+        // Above generic means I can pass any collection of ThirdStudent or LPAStudent.
+        StringBuilder sb = new StringBuilder();
+        for (var student : students) {
+            sb.append(student.getYearStarted()).append(": ").append(student).append("\n");
+        }
+        sb.delete(0, sb.length());
+        return sb.toString();
     }
 }
