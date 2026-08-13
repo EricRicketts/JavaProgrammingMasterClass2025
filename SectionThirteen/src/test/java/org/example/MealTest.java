aa3c88fbb637840db5ec0ea1918907b9edd89aec
EricRicketts <eric_ricketts@icloud.com>
2026-08-13 08:04:27 -0400

    get and set burger fields from a meal instance

--- SectionThirteen/src/test/java/org/example/MealTest.java
+++ SectionThirteen/src/test/java/org/example/MealTest.java
@@ -1,8 +1,13 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MealTest {
 
@@ -12,5 +17,54 @@ public class MealTest {
     public void setUp() {
         firstMeal = new Meal();
         secondMeal = new Meal(new BigDecimal("0.50"));
+    }
+
+    @Nested
+    @DisplayName("test first meal burger meat type, size and price")
+    class TestFirstMealBurgerMeatTypeSizeAndPrice {
+
+        @Test
+        public void testFirstMealGetBurgerMeatType() {
+            assertEquals(BurgerMeatType.ROUND, firstMeal.getBurger().getMeatType());
+        }
+
+        @Test
+        public void testFirstMealGetBurgerSize() {
+            assertEquals(BurgerSize.MEDIUM, firstMeal.getBurger().getSize());
+        }
+
+        @Test
+        public void testFirstMealGetBurgerPrice() {
+            assertEquals(new BigDecimal("2.55"), firstMeal.getBurger().getPrice());
+        }
+    }
+
+    @Nested
+    @DisplayName("test set burger meat type, size and price")
+    class TestFirstMealSetBurgerMeatTypeSizeAndPrice {
+
+        @Test
+        public void testFirstMealSetBurgerMeatType() {
+            assertEquals(BurgerMeatType.ROUND, firstMeal.getBurger().getMeatType());
+
+            firstMeal.getBurger().setMeatType(BurgerMeatType.SIRLOIN);
+            assertEquals(BurgerMeatType.SIRLOIN, firstMeal.getBurger().getMeatType());
+        }
+
+        @Test
+        public void testFirstMealSetBurgerSize() {
+            assertEquals(BurgerSize.MEDIUM, firstMeal.getBurger().getSize());
+
+            firstMeal.getBurger().setSize(BurgerSize.LARGE);
+            assertEquals(BurgerSize.LARGE, firstMeal.getBurger().getSize());
+        }
+
+        @Test
+        public void testFirstMealSetBurgerPrice() {
+            assertEquals(new BigDecimal("2.55"), firstMeal.getBurger().getPrice());
+
+            firstMeal.getBurger().setPrice(new BigDecimal("2.75"));
+            assertEquals(new BigDecimal("2.75"), firstMeal.getBurger().getPrice());
+        }
     }
 }
