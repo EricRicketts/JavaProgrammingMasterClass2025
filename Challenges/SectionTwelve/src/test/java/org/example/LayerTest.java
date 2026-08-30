36b1dcd3d6058085b3b7bb838be5e5bbedb4303d
EricRicketts <eric_ricketts@icloud.com>
2026-08-30 16:23:36 -0400

    check in changes so files can be deleted

--- Challenges/SectionTwelve/src/test/java/org/example/LayerTest.java
+++ Challenges/SectionTwelve/src/test/java/org/example/LayerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
